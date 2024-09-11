8c5a7d537e281ca6928762d362f8e6904a2fe761
sea <45450994+warmthsea@users.noreply.github.com>
2024-09-11 17:40:17 +0800

    Update button.vue

--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -7,7 +7,7 @@
     :style="buttonStyle"
     @click="handleClick"
   >
-    333
+    444
     <template v-if="loading">
       <slot v-if="$slots.loading" name="loading" />
       <el-icon v-else :class="ns.is('loading')">
